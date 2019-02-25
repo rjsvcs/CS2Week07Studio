2fe58766383f3a51b68406c0d241290f1e8ecdf4
rjsvcs <rjsvcs@rit.edu>
2019-02-24 19:56:50 -0500

    Added documentation to the Square class.

--- src/reversi/Square.java
+++ src/reversi/Square.java
@@ -1,13 +1,27 @@
 package reversi;
 
+/**
+ * A class that represents a square on a board in a game of Reversi.
+ */
 public class Square {
 
+    /**
+     * The piece that may or may not occupy the square.
+     */
     private Piece piece;
 
+    /**
+     * Creates a new, empty square.
+     */
     public Square() {
         piece = null;
     }
 
+    /**
+     * Creates a square with a piece corresponding to the specified symbol.
+     *
+     * @param symbol Either 'B' for black, 'W' for white, or ' ' for empty.
+     */
     public Square(char symbol) {
         Color color = Color.getColor(symbol);
         if(color != null) {
@@ -15,10 +29,24 @@ public class Square {
         }
     }
 
+    /**
+     * Returns true if the square has a piece played on it, and false
+     * otherwise.
+     *
+     * @return True if a piece has been played on the square. Otherwise false.
+     */
     public boolean isOccupied() {
         return piece != null;
     }
 
+    /**
+     * Attempts to play a piece on the square.
+     *
+     * @param piece The piece being played on the square.
+     *
+     * @throws BadMoveException If a piece has already been played on the
+     * square.
+     */
     public void occupy(Piece piece) throws BadMoveException {
         if(isOccupied()) {
             throw new BadMoveException("Square is already occupied");
@@ -27,10 +55,18 @@ public class Square {
         }
     }
 
+    /**
+     * Resets the square to the empty state (no piece played).
+     */
     public void reset() {
         piece = null;
     }
 
+    /**
+     * Flips the piece played on this square to the opposite side.
+     *
+     * @throws BadMoveException If the square is empty.
+     */
     public void flip() throws BadMoveException {
         if(!isOccupied()) {
             throw new BadMoveException("Square is empty.");
@@ -39,6 +75,12 @@ public class Square {
         }
     }
 
+    /**
+     * Returns a string representation of the square; either " " if the square
+     * is empty, or the short symbol for the piece if it is not.
+     *
+     * @return A string representation of the square.
+     */
     @Override
     public String toString() {
         return isOccupied() ? piece.toString() : " ";
